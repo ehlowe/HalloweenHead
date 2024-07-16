a6c1add24a01bb9800a3cc57f73cb76e28273d67
Ethan Lowe <104944142+MrJellyBean3@users.noreply.github.com>
2024-07-16 02:33:26 -0500

    Significant Conversational Behavioral Refactoring

--- ConversationalBehavioral.py
+++ ConversationalBehavioral.py
@@ -11,8 +11,6 @@ import openai
 from openai import OpenAI
 import time
 from pvrecorder import PvRecorder
-import wave
-import struct
 import numpy as np
 
 
@@ -23,9 +21,6 @@ import customlibrary.HalloweenHead as hh
 # Socket connection
 def socket_connection(shared_dict):
     import socket
-    import select
-
-    print(socket.gethostname())
 
     hostname = socket.gethostname()
     port=1234
@@ -47,6 +42,7 @@ def socket_connection(shared_dict):
 
     while True:
         try:
+            # Process the position and inference data coming from the socket
             data=conn_obj.recv(1024)
             data_string=data.decode()
             if data_string!="":
@@ -58,6 +54,7 @@ def socket_connection(shared_dict):
                 else:
                     shared_dict["failed"]=False
 
+            # Send the mouth and eye data to the socket
             mouth=shared_dict["mouth"]
             eye=shared_dict["eye"]
             send_message=f"{str(mouth)},{str(eye)}\n"
@@ -82,8 +79,7 @@ def socket_connection(shared_dict):
                 
                 fail_check_time=time.time()
 
-
-            
+        # If there is an error, try to reconnect
         except Exception as e:
             print("read or send error: ", e, " line: ", e.__traceback__.tb_lineno)
             try:
@@ -95,6 +91,8 @@ def socket_connection(shared_dict):
 # Vision Description
 def description_task(shared_dict):
     import cv2
+
+    # Get the image to describe and write it to a file
     while True:
         im=cv2.imread("working_files/shi.jpg")
         initial_size = os.path.getsize("working_files/shi.jpg")
@@ -103,11 +101,14 @@ def description_task(shared_dict):
             cv2.imwrite("working_files/vision_description_image.jpg",im)
             break
 
+    # Run llava to describe the image
     vision_output = replicate.run(
         "yorickvp/llava-13b:2facb4a474a0462c15041b78b1ad70952ea46b5ec6ad29583c0b29dbd4249591",
         input={"image": open("working_files/vision_description_image.jpg", "rb"),"prompt":"In this image is: "}
     )
     description_string=""
+
+    # Update the shared dictionary with the vision description
     for item in vision_output:
         description_string+=item
         shared_dict["visual_description"]=description_string
@@ -117,15 +118,11 @@ def description_task(shared_dict):
 
 
 if __name__ == "__main__":
+    # Ensure the working directories exist
     if not os.path.isdir("working_files/audio_files"):
         os.mkdir("working_files/audio_files")
     if not os.path.isdir("working_files/tts_files"):
         os.mkdir("working_files/tts_files")
-
-    #create thread for motion
-    import threading
-    import keyboard
-    import cv2
 
     # Read all keys
     with open("working_files/SECRET.txt",'r') as f:
@@ -133,32 +130,40 @@ if __name__ == "__main__":
         OPENAI_SECRET=list_of_keys[0]
         TOGETHER_SECRET=list_of_keys[1]
         REPLICATE_SECRET=list_of_keys[2]
-    
+
     # Setup OpenAI for transcription
     os.environ['OPENAI_API_KEY'] = OPENAI_SECRET
     openai.api_key=OPENAI_SECRET
-    from concurrent.futures import ThreadPoolExecutor, TimeoutError
 
-    # Setup together LLM
-    model_name="togethercomputer/llama-2-13b-chat"
-    together_model_name="togethercomputer/llama-2-70b-chat"
+    # Setup Together API key
     import together
     together.api_key = TOGETHER_SECRET
+    model_name="togethercomputer/llama-2-13b-chat"
+    together_model_name="togethercomputer/llama-2-70b-chat"
 
     # Setup replicate Vision LLM
     import replicate
     os.environ["REPLICATE_API_TOKEN"]=REPLICATE_SECRET
 
+    chatbot_enabled=True
+
+    # Import the necessary libraries
+    import keyboard
+    import cv2
+    from concurrent.futures import ThreadPoolExecutor, TimeoutError
+    import base64
+
     # Setup shared data 
     manager = multiprocessing.Manager()
     shared_dict = manager.dict()
+
     #Audio
     shared_dict["audio"]=[]
     shared_dict["savebool"]=True
     shared_dict["saveduration"]=0
     shared_dict["playingsound"]=True
 
-    #TTs
+    #TTS
     shared_dict["text"]=""
     shared_dict["file queue"]=[]
     shared_dict["last_token_size"]=0
@@ -169,10 +174,10 @@ if __name__ == "__main__":
     shared_dict["mouth"]=0
     shared_dict["eye"]=1
 
-    # Vision
+    # Vision Description
     shared_dict["visual_description"]=""
 
-    # Motion
+    # Motion/Position
     shared_dict["x_pos"]=None
     shared_dict["y_pos"]=None
     shared_dict["failed"]=True
@@ -180,11 +185,9 @@ if __name__ == "__main__":
     shared_dict["at_door"]=False
 
 
-
+    # Start the socket thread
     socket_thread = threading.Thread(target=socket_connection, args=(shared_dict,))
     socket_thread.start()
-
-    being_smart=True
 
     #start threads
     recording_thread = threading.Thread(target=hh.recorder, args=(shared_dict,))
@@ -198,23 +201,22 @@ if __name__ == "__main__":
     tts_thread=threading.Thread(target=hh.tts_generator,args=(shared_dict,))
     tts_thread.start()
 
-
-
     chat_log=""
 
     path_i=0
-    #while recording thread is running, do this
+
+    # Variables for monitoring trick or treater behavior
     approaching_to_door=False
     prev_approaching_to_door=False
     approach_ready=False
 
     t_print=time.time()
-    off_bool=False
 
     t_failed_start=time.time()
 
     ran_vision_description=False
 
+    # LLM model selector
     openai_bool=False
     together_bool=True
     if openai_bool:
@@ -222,15 +224,12 @@ if __name__ == "__main__":
     if together_bool:
         openai_bool=False
 
-    import base64
-    import os
-
-
-
+    # Encode to base64 for vision model to accept
     def encode_image(image_path):
         with open(image_path, "rb") as image_file:
             return base64.b64encode(image_file.read()).decode('utf-8')
 
+    # Function to get response from OpenAI
     def get_openai_response(the_prompt):
         client = OpenAI()
         image_path="working_files/vision_description_image.jpg"
@@ -264,68 +263,53 @@ if __name__ == "__main__":
     while recording_thread.is_alive():
         time.sleep(0.05)
 
+        # If the trick or treater is approaching the door, update the approach_ready variable
+        prev_approaching_to_door=approaching_to_door
         if shared_dict["approaching"]:
             approaching_to_door=True
 
-        if not ran_vision_description:
-            if (time.time()-t_failed_start)>0.4:
-                description_thread=threading.Thread(target=description_task, args=(shared_dict,))
-                #description_thread.start()
-                ran_vision_description=True
-        
-        prev_approaching_to_door=approaching_to_door
-
+        # If approaching to door and now at door start the bot
         if approaching_to_door:
             if shared_dict["at_door"]:
                 approach_ready=True
                 approaching_to_door=False
 
+        # If it has been more than 2 seconds since seeing someone reset the chat and behavior
         if shared_dict["failed"]:
             if (time.time()-t_failed_start)>2:
                 approach_ready=False
                 shared_dict["savebool"]=True
                 chat_log=""
                 t_failed_start=time.time()
-                ran_vision_description=False
         else:
             t_failed_start=time.time()
 
+        # Manually start the bot
         if keyboard.is_pressed('r'):
             approach_ready=True
             print("\nAPPROACH READY ON\n")
-        # if keyboard.is_pressed('c'):
-        #     off_bool=True
-        # if keyboard.is_pressed('v'):
-        #     off_bool=False
 
+        # Periodically print the status of the trick or treater
         if time.time()-t_print>1:
             print("Failed: ", shared_dict['failed'], "Approaching: ", shared_dict["approaching"], "At Door: ", shared_dict["at_door"], "Approach Ready: ", approach_ready," Approaching to door: ", approaching_to_door)
             t_print=time.time()
 
-        # if off_bool:
-        #     shared_dict["text"]=""
-        #     shared_dict["savebool"]=False
-
-        #if recording thread has saved audio, do this
-        if shared_dict["savebool"] and approach_ready and not off_bool:
-            if not ran_vision_description:
-                description_thread=threading.Thread(target=description_task, args=(shared_dict,))
-                #description_thread.start()
-                ran_vision_description=True
+        #if recording thread has saved audio
+        if shared_dict["savebool"] and approach_ready:
             shared_dict["playingsound"]=True
             path_i+=1
             shared_dict["savebool"]=False
             hh.save_audio(shared_dict["audio"],shared_dict["saveduration"],"working_files/audio_files/"+str(path_i)+".wav")
             print(shared_dict["saveduration"]," Saving audio ", path_i)
 
-            #stop listening
+
+
             t_thinking_start=time.time()
-
-
-            if being_smart:
-                #transcribe
+            if chatbot_enabled:
+                # Get what the trick or treater said
                 t_start=time.time()
                 if chat_log!="":
+                    # set eye state to thinking (blue)
                     shared_dict["eye"]=2
                     audio_file= open("working_files/audio_files/"+str(path_i)+".wav", "rb")
                     try:
@@ -339,8 +323,8 @@ if __name__ == "__main__":
                 t_end=time.time()
                 print("Time to transcribe: ",t_end-t_start, " Text:",transcript["text"])
 
+                # Set prompt
                 ts=time.time()
-                # generate response
                 if chat_log=="":
                     the_prompt="I want you to role play as a talking skeleton head who is whitty and funny. You are greeting trick-or-treaters on Halloween night at the front door of a house. I want this conversation to be engauging for the trick or treaters and for it to be funny. Try to be a little crazy and try not to repeat yourself or say things that are repetitive and don't ramble on for a long time because this is a back and forth conversation. This is what you see in front of you: "+shared_dict["visual_description"]
                     chat_log=the_prompt
@@ -351,14 +335,10 @@ if __name__ == "__main__":
                 
                 #open a yaml file to write the prompt to the end
                 with open("working_files/prompt.yaml","a") as f:
-                    #get rid of emojis from prompt
                     write_prompt=the_prompt.encode('ascii', 'ignore').decode('ascii')
                     f.write(write_prompt+"\n\n\n\n\n")
 
-                print("the prompt test: ",the_prompt)
-                #the_prompt="I've seen more candy in a single bag! But, hey"
                 ts=time.time()
-                # generate response
                 result_l=[]
                 shared_dict["file queue"]=[]
                 shared_dict["time_started"]=time.time()
@@ -369,6 +349,8 @@ if __name__ == "__main__":
 
                 timeout_seconds = 2
                 response=None
+
+                # Response for OpenAI model
                 if openai_bool:
                     while response==None:
                         with ThreadPoolExecutor(max_workers=1) as executor:
@@ -377,9 +359,9 @@ if __name__ == "__main__":
                                 response = future.result(timeout=timeout_seconds)
                             except TimeoutError:
                                 print(f"Timeout after {timeout_seconds} seconds")
-                                response = None
+                                response = "time out"
 
-                    print("MADE RESPONSE")
+                    # Send the response to the shared dictionary as it comes in
                     collected_messages=[]
                     for chunk in response:
                         try:
@@ -389,19 +371,11 @@ if __name__ == "__main__":
                             shared_dict["text"]="".join(collected_messages)
                         except Exception as e:
                             print(e)
-                    result_s="".join(collected_messages)
+                    result_s="".join(collected_messages)                
+                print("LLM RESPONSE: ",result_s)
 
 
-                if together_bool:
-                    # Together llamma response:
-                    for token in together.Complete.create_streaming(prompt=the_prompt,model=together_model_name,max_tokens=500,temperature=0.9):
-                        print(token, end="", flush=True)
-                        shared_dict["last_token_size"]=len(token)
-                        result_l.append(token)
-                        result_s="".join(result_l).strip()
-                        shared_dict["text"]=result_s
-                print("LLM RESULT: ",result_s)
-
+                # Set that the LLM has finished
                 shared_dict["llm_finished"]=True
                 chat_log=chat_log+"\nYou: "+result_s.split("You: ")[-1].split("</s>")[0]
                 with open("working_files/prompt.yaml","a") as f:
